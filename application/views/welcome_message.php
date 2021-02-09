0004b7bde313d8a86aa3ebe2d2720d779d61a1dc
Edward Riley <55459510+emr9018@users.noreply.github.com>
2021-02-09 18:48:25 -0500

    MySQL Integration Test

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,5 +1,34 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
+<?php
+$servername = "localhost";
+$username = "pi";
+$password = "password";
+$dbname = "school";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT test FROM test_table";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "value: " . $row["test"];
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
