6168ec877ae8dd0d9f01e48ba338792c5bb78895
Edward Riley <55459510+emr9018@users.noreply.github.com>
2021-02-09 20:16:42 -0500

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -3,32 +3,6 @@
 
 phpinfo();
 
-$servername = "localhost";
-$username = "pi";
-$password = "password";
-$dbname = "school";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT test FROM test_table";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "value: " . $row["test"] . "<br>";
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
-
-
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
