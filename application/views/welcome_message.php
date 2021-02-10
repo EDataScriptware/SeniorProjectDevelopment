ebd1a6bfb0f20ca365466220dd952904f5a3a5a5
Edward Riley <55459510+emr9018@users.noreply.github.com>
2021-02-09 19:01:30 -0500

    retesting mysqli

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,8 +1,30 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$mysqli = new mysqli();
 
-echo $mysqli->connect_error();
+$servername = "localhost";
+$username = "pi";
+$password = "password";
+$dbname = "school";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT test FROM test_table";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "value: " . $row["test"] . "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
 
 
 ?><!DOCTYPE html>
