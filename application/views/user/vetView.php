4570b584709c205e7f850ff8c733e5debc7e4c0f
0rangeBoy <demonsonicth@gmail.com>
2021-03-02 14:33:55 -0500

    casdc

--- application/views/user/vetView.php
+++ application/views/user/vetView.php
@@ -1,8 +1,8 @@
 <p> vet view </p>
 
-<?php console.log (json_encode($veteran))?>
+<?php console_log (json_encode($veteran))?>
 
-<h2> <?php echo $veteran[0]->first_name $veteran[0]->middle_initial $veteran[0]->last_name ?> </h2>
+<h2> <?php echo $veteran[0]->first_name ?> <?php echo $veteran[0]->middle_initial ?>  <?php echo $veteran[0]->last_name ?> </h2>
 
 <p>Contact Info: <?php echo $veteran[0]->cell_phone ?></p>
 <p>dob: <?php echo $veteran[0]->dob ?></p>
