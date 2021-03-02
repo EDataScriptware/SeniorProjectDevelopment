461b193819fd8f5e9e655a4dde8716dba3c203ee
0rangeBoy <demonsonicth@gmail.com>
2021-03-02 14:11:37 -0500

    test

--- application/views/user/vetList.php
+++ application/views/user/vetList.php
@@ -7,17 +7,17 @@
     <th>Firstname</th>
     <th>Lastname</th>
     <th>id</th>
+    <th>Action </th>
   </tr>
 
 <?php foreach ($veteran as $vet): ?>
-
-    <!-- <a href = '' > -->
-  <tr class = "clickable-row" data-href="<?php echo base_url('vetView'. '/'. $vet->veteran_id) ?>">
+    
+  <tr class = "clickable-row">
     <td><?php echo $vet->first_name ?></td>
     <td><?php echo$vet->last_name?></td>
     <td><?php echo$vet->veteran_id?></td>
+    <td><button href="<?php echo base_url('vetView'. '/'. $vet->veteran_id) ?>" type="button" class="btn btn-primary">Primary</button></td>
   </tr>
-<!-- </a> -->
 
 <?php endforeach ?>
 
