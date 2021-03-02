d7af2a09d169a97961bcbe12050663e56f59effb
Edward Riley <55459510+emr9018@users.noreply.github.com>
2021-03-02 13:28:39 -0500

    id_post raw code

---
+++ application/views/user/id-post.php
@@ -0,0 +1,9 @@
+<?php
+if ( isset( $_POST['submit'] ) ) {
+
+$identifier = $_POST['identifier'];
+
+echo "Is my identifier number" . $identifier . " working?";
+
+}
+?>
--- application/views/user/index.php
+++ application/views/user/index.php
@@ -1,1 +1,7 @@
 <p> user </p>
+
+<form action="id-post.php" method="post">
+  <label for="identifier">ID Lookup:</label>
+  <input type="text" id="identifier" name="identifier"><br><br>
+  <input type="submit" value="Search ID">
+</form>
