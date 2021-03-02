995a8d98b721a7f2c7eb0592c60cc3afc8c5adb3
0rangeBoy <demonsonicth@gmail.com>
2021-03-02 13:51:20 -0500

    test

--- application/views/user/vetList.php
+++ application/views/user/vetList.php
@@ -1,3 +1,24 @@
 
 
-<?php echo json_encode($veteran)?>
+<?php // echo json_encode($veteran)?>
+
+<table id = 'userList'>
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th>
+    <th>id</th>
+  </tr>
+
+<?php foreach ($veteran as $vet): ?>
+
+    <a href = <?php base_url('vetView/'$vet->veteran_id) ?>>
+  <tr>
+    <td><?php$vet->first_name ?></td>
+    <td><?php$vet->last_name?></td>
+    <td><?php$vet->veteran_id?></td>
+  </tr>
+</a>
+
+</table>
+
+<?php endforeach ?>
