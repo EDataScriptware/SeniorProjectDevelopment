f07581150c900f58025ab933c54f4f6215818341
0rangeBoy <demonsonicth@gmail.com>
2021-03-25 17:17:32 -0400

    test

--- application/views/user/vetList.php
+++ application/views/user/vetList.php
@@ -15,6 +15,19 @@
 <?php foreach ($veteran as $vet): ?>
 	<a href="<?php echo base_url('vetView'. '/'. $vet->veteran_id) ?>" class="teamListElement"><?php echo $vet->first_name ?> <?php echo$vet->last_name?></a>
 <?php endforeach ?>
-<?php } ?>
+<?php } else { ?>
+
+<?php foreach ($team as $tem): ?>
+
+	<h3> <?php echo $tem->color ?> Team View </h3>
+
+<?php foreach ($veteran as $vet): ?>
+	<?php if ($vet->team_id === $tem->team_id) { ?>
+	<a href="<?php echo base_url('vetView'. '/'. $vet->veteran_id) ?>" class="teamListElement"><?php echo $vet->first_name ?> <?php echo$vet->last_name?></a>
+	<?php  }?>
+<?php endforeach ?>
+
+<?php endforeach ?>
+	<?php } ?>
 </div>
 
