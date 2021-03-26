43febe2fe0d1573e3e1b8fda561055a185c87b68
Zach <zacheas@outlook.com>
2021-03-25 21:33:08 -0400

    something

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -12,14 +12,14 @@
         foreach($vetData as $vet) {
             echo "<tr>" ;
             ?>
-            <form id='<?php $vet->veteran_id ?>' class='vet_entry_edit' method='post' action='<?php echo base_url('Admin/updateVet/'); ?>'>;
+            <td><form id='<?php $vet->veteran_id ?>' class='vet_entry_edit' method='post' action='<?php echo base_url('Admin/updateVet/'); ?>'></td>;
             <?php
 
             foreach($vet as $key => $value) {
                 echo  "<td><input form=".$vet->veteran_id." type='text' name='".$key."' value='".$value."'></td>" ;
             }
 
-            echo  "<button input form='".$vet->veteran_id."' type='submit' name='submit' value='Submit'> Submit </button>";
+            echo  "<td><button input form='".$vet->veteran_id."' type='submit' name='submit' value='Submit'> Submit </button></td>";
             echo "</form>";
             echo "</tr>" ;
         }
