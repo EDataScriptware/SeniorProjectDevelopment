ee56fbf5a6190db4ff30115ea4588cf8e20bb160
Zach <zacheas@outlook.com>
2021-03-25 21:25:43 -0400

    syntax

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -11,14 +11,14 @@
     <?php
         foreach($vetData as $vet) {
             echo "<tr>" ;
-            echo "<td><form id='".$vet->veteran_id."' class='vet_entry_edit' method='post' action='". base_url('Admin/updateVet/'); . "'/>";
+            echo "<form id='".$vet->veteran_id."' class='vet_entry_edit' method='post' action='". base_url('Admin/updateVet/'); . "'/>";
 
             foreach($vet as $key => $value) {
-                echo  "<input form=".$vet->veteran_id." type='text' name='".$key."' value='".$value."'>" ;
+                echo  "<td><input form=".$vet->veteran_id." type='text' name='".$key."' value='".$value."'></td>" ;
             }
 
             echo  "<button input form='".$vet->veteran_id."' type='submit' name='submit' value='Submit'> Submit </button>";
-            echo "</form></td>";
+            echo "</form>";
             echo "</tr>" ;
         }
     ?>
