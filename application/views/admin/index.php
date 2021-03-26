9841eade36dd18060786985b423c537881d1911b
Zach <zacheas@outlook.com>
2021-03-25 21:00:27 -0400

    logic

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -3,11 +3,25 @@
 <table>
     <tr>
         <?php
+            echo json_encode($vetData);
+            echo "<br/>";
+            echo json_encode($fields);
+            echo "<br/>";
+
             foreach($fields as $field) {
                 echo "<th>".$field."</th>";
             }
         ?>
     </tr>
-<?php
-?>
+    <?php
+        foreach($vetData as $vet) {
+            echo "<tr>" ;
+
+            foreach($vet as $data) {
+                echo "<td>". $data . "</td>";
+            }
+
+            echo "</tr>" ;
+        }
+    ?>
 </table>
