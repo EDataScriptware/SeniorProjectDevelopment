d25ee4510a467784744dd7d3213b07f858e689ce
Zach <zacheas@outlook.com>
2021-03-29 20:16:03 -0400

    again?

--- application/models/Veteran_model.php
+++ application/models/Veteran_model.php
@@ -98,8 +98,8 @@ class Veteran_model extends CI_Model {
     
         try
         {
-            $this->db->where('veteran_id', $vetID);
             $this->db->join('bus', 'bus.bus_id = veteran.bus_id');
+            $this->db->where('veteran_id', $vetID);
             $this->db->update('veteran', $vet); // gives UPDATE `mytable` SET `field` = 'field+1' WHERE `id` = 2
             $bool = true;
         } 
