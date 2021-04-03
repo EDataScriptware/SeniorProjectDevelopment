482aced46647d17eb43641b400a163b187481750
0rangeBoy <demonsonicth@gmail.com>
2021-04-03 17:53:55 -0400

    t

--- application/views/user/vetView.php
+++ application/views/user/vetView.php
@@ -121,9 +121,9 @@
 </div>
 
 <!-- Modal -->
-<div class="modal fade " id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+<div class="modal fade modal-xl  " id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
   <div class="modal-dialog modal-xl " role="document">
-    <div class="modal-content">
+    <div class="modal-content modal-xl">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLongTitle">Modify Veteran Information</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
