99ccf19230bcf6749c0b951676c5a0b7e4703042
0rangeBoy <demonsonicth@gmail.com>
2021-04-05 02:40:27 -0400

    t

--- application/views/admin/users.php
+++ application/views/admin/users.php
@@ -83,6 +83,12 @@
             alert($result);
             document.getElementById("whiteEdit").style.width = "550px";
             document.getElementById("whiteEdit").style.padding = "0px 0px 00px 60px";
+            
+            document.getElementById("username").value = $result->username;
+            document.getElementById("user_type").value = $result->user_type;
+
+            
+
         });       
         }
 
