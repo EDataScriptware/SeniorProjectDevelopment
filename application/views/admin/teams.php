c2c944c7bc511e3283aed0dd990cab558519b5bb
0rangeBoy <demonsonicth@gmail.com>
2021-04-06 15:16:59 -0400

    t

--- application/views/admin/teams.php
+++ application/views/admin/teams.php
@@ -1,12 +1,59 @@
 <?php foreach ($team as $tem): ?>
 
-<h3> <?php echo $tem->color ?> Team  </h3>
+    <?php
+	$this->db->select("*");
+	$this->db->from('bus');
+	$this->db->where('bus_id',$tem->bus_id);
+
+	$bus = $this->db->get()->result();
+	?>
+
+<h3> <?php echo $tem->color ?> Team - <?php $bus[0]->name?>  </h3>
 
 <script>
     $(document).ready( function () {
     $('#<?php echo $tem->color ?>Vet').DataTable();
+    $('#<?php echo $tem->color ?>User').DataTable();
 } );
     </script>
+<h2> Staff </h2>
+
+<div class = "scrunch"> 
+<table id="<?php echo $tem->color ?>User"  class="table table-striped table-bordered">
+    <thead>
+        <tr>
+            <th></th>
+            <th>Veteran Contact Info</th>
+            <th>Guardian</th>
+            <th>Guardian Contact Info</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($veteran as $vet): ?>
+
+    <?php
+	$this->db->select("*");
+	$this->db->from('guardian');
+	$this->db->where('guardian_id',$vet->guardian_id);
+
+	$guardian = $this->db->get()->result();
+	?>
+    
+        <tr>
+            <td> <?php echo $vet->first_name ?> <?php echo$vet->last_name?></td>
+            <td> <?php echo 'Day_Phone: '.$vet->day_phone ?> <br> <?php echo 'Cell_Phone: '.$vet->cell_phone?> </td>
+            <td> <?php if ($guardian != null) { echo $guardian[0]->first_name." ".$guardian[0]->last_name; } else {echo "None";}?></td>
+            <td> <?php if ($guardian != null) { echo'Day_Phone: '.$guardian[0]->day_phone.'<br> Cell_Phone: '.$guardian[0]->cell_phone; } else {echo "None";}?> </td>
+            <td> <button type="button" class="btn btn-primary" onclick = "moveBlock(<?php echo $vet->veteran_id ?>)"  > MOVE </button> <button type="button" class="btn btn-primary" onclick = "moveBlock(<?php echo $vet->veteran_id ?>)"  > REMOVE </button> </td>
+
+        </tr>
+        <?php endforeach ?>
+    </tbody>
+</table>
+</div>
+
+
 <h2> Veterans </h2>
 <div class = "scrunch"> 
 <table id="<?php echo $tem->color ?>Vet"  class="table table-striped table-bordered">
@@ -32,9 +79,9 @@
     
         <tr>
             <td> <?php echo $vet->first_name ?> <?php echo$vet->last_name?></td>
-            <td> <?php echo 'Day_Phone: '.$vet->day_phone ?> <br> <?php echo 'Cell_Phone: '.$vet->cell_phone?> </td>
+            <td> <?php echo 'Day Phone: '.$vet->day_phone ?> <br> <?php echo 'Cell Phone: '.$vet->cell_phone?> </td>
             <td> <?php if ($guardian != null) { echo $guardian[0]->first_name." ".$guardian[0]->last_name; } else {echo "None";}?></td>
-            <td> <?php if ($guardian != null) { echo'Day_Phone: '.$guardian[0]->day_phone.'<br> Cell_Phone: '.$guardian[0]->cell_phone; } else {echo "None";}?> </td>
+            <td> <?php if ($guardian != null) { echo'Day Phone: '.$guardian[0]->day_phone.'<br> Cell Phone: '.$guardian[0]->cell_phone; } else {echo "None";}?> </td>
             <td> <button type="button" class="btn btn-primary" onclick = "moveBlock(<?php echo $vet->veteran_id ?>)"  > MOVE </button> <button type="button" class="btn btn-primary" onclick = "moveBlock(<?php echo $vet->veteran_id ?>)"  > REMOVE </button> </td>
 
         </tr>
