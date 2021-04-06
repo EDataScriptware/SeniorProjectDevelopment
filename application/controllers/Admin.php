c594bf8c6a4102a99bf9d2c541ebb0579634bf61
0rangeBoy <demonsonicth@gmail.com>
2021-04-05 22:31:50 -0400

    t

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -203,7 +203,7 @@ class Admin extends CI_Controller {
 		$this->load->model('Guardian_model');
 		$id = $this->input->post('id');
 		
-		$data = $this->Veteran_model->get_one_guardian($id);
+		$data = $this->Guardian_model->get_one_guardian($id);
 		
 		echo json_encode($data);
 	}
