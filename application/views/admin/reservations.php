586215c0a789c9276699af55ebc42f1c37d6c722
0rangeBoy <demonsonicth@gmail.com>
2021-04-12 18:23:33 -0400

    t

--- application/views/admin/reservations.php
+++ application/views/admin/reservations.php
@@ -118,7 +118,7 @@ $(document).ready( function () {
 <datalist id ='veterans'>
 <?php $skip = false ?>
 <?php foreach ($veteran as $vet): ?>
-
+    <option value='<?php echo $vet->veteran_id ?>'> <?php echo $vet->first_name ?> <?php echo $vet->last_name ?></option>
     <?php foreach ($hotel as $hot): ?>
      <?php if ($hot->$veteran_id === $vet->veteran_id) {
          $skip = true;
@@ -128,7 +128,7 @@ $(document).ready( function () {
 
     <?php if ($skip = false) {?>
         <option value='<?php echo $vet->veteran_id ?>'> <?php echo $vet->first_name ?> <?php echo $vet->last_name ?></option>
-        <?php   }else {$skip = false;} ?>
+        <?php }else {$skip = false;} ?>
 <?php endforeach ?>
 </datalist>
 
