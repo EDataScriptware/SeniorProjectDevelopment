0c85528ec89ff0b66b689d4a932b03056835ff33
Zach <zacheas@outlook.com>
2021-04-12 15:20:28 -0400

    update

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -32,7 +32,7 @@ class Admin extends CI_Controller {
 			$bus_data = $this->Bus_model->get_mission_bus_data($bus_book->mission_id);
 
 			if($bus_data) {
-				$bus_book->append($bus_data);
+				$bus_book->bus = $bus_data;
 				array_push($data['bus_book_data'], $bus_book);
 			}
 			else {
