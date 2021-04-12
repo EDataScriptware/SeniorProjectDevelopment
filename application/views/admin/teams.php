c3363304b24a5a15c8d04245196ade4c587e9e52
0rangeBoy <demonsonicth@gmail.com>
2021-04-12 02:38:02 -0400

    t

--- application/views/admin/teams.php
+++ application/views/admin/teams.php
@@ -294,7 +294,7 @@ $unVetCheck = false;
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Add a bus</h5>
+        <h5 class="modal-title">Add a Team</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
