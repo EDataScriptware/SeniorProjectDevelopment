fbbe6791a32e7cc638a6e5f667798b112998b865
Zach <zacheas@outlook.com>
2021-04-12 16:44:34 -0400

    update

--- application/views/admin/upload_success.php
+++ application/views/admin/upload_success.php
@@ -5,15 +5,13 @@
 <body>
 
 <h3>Your file was successfully uploaded!</h3>
-<!-- <h3>Return to the "documents" page to view your file</h3> -->
+<h3>Return to the "documents" page to view your file or to upload another.</h3>
 
 <ul>
 <?php foreach ($upload_data as $item => $value):?>
 <li><?php echo $item;?>: <?php echo $value;?></li>
 <?php endforeach; ?>
 </ul>
-
-<p><?php echo anchor('admin/documents', 'Upload Another File!'); ?></p>
 
 </body>
 </html>
