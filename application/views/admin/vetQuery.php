a2f5a740b9cdffd8cf08e6993f030a1f6762368d
Zach <zacheas@outlook.com>
2021-04-13 17:48:07 -0400

    update

--- application/views/admin/vetQuery.php
+++ application/views/admin/vetQuery.php
@@ -1,5 +1,5 @@
 <script> $(document).ready( function () {  
-    $('#doc').addClass('active');
+    $('#home').addClass('active');
     $('#vetTable').DataTable();
 } ); 
 </script>
@@ -7,27 +7,28 @@
 <p> Vet Query </p>
 
 <table id="vetTable"  class="table table-striped table-bordered">
-    <tr>
-        <?php
-            foreach($fields as $field) { ?>
-                <th><?php echo $field ?></th>
-        <?php } ?>
-    </tr>
-    <tr>
-        Action
-    </tr>
-    <?php
-        foreach($vetData as $vet) {?>
+    <thead>
+        <tr>
+            <?php
+                foreach($fields as $field) { ?>
+                    <th><?php echo $field ?></th>
+            <?php } ?>
+        </tr>
+        <tr>
+            Action
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($vetData as $vet) {?>
             <tr>
                 <form id='<?php echo $vet->veteran_id ?>' class='vet_entry_edit' method='post' action='<?php echo base_url('Admin/updateVet/'); ?>'>
-        
-            <?php
-            foreach($vet as $key => $value) { ?>
-                <td><?php echo "<input form=".$vet->veteran_id." type='text' name='".$key."' value='".$value."'>" ;?></td>
-            <?php } ?>
+                    <?php foreach($vet as $key => $value) { ?>
+                        <td><?php echo "<input form=".$vet->veteran_id." type='text' name='".$key."' value='".$value."'>" ;?></td>
+                    <?php } ?>
 
-                <td><?php echo "<button input form='".$vet->veteran_id."' type='submit' name='submit' value='Submit'> Submit </button>"; ?> </td>
+                    <td><?php echo "<button input form='".$vet->veteran_id."' type='submit' name='submit' value='Submit'> Submit </button>"; ?> </td>
                 </form>
             </tr>
         <?php } ?>
+    </tbody>
 </table>
