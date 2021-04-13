4fdcaa27f1c9a39c2ee475f07219653286ccf478
Zach <zacheas@outlook.com>
2021-04-13 19:49:47 -0400

    bus book

--- application/views/admin/busbook.php
+++ application/views/admin/busbook.php
@@ -1,3 +1,40 @@
+<script> $(document).ready( function () {  
+    $('#book').addClass('active');
+} ); 
+
+<?php foreach($bus_book_data as $bus) { ?>
+    $('#<?php echo $bus->bus_book_id; ?>book').DataTable();
+<?php } ?> 
+
+</script>
+
 <?php 
     echo json_encode($bus_book_data) ;
+
+    if(isset($bus_book_data)) {
+    foreach($bus_book_data as $book) { ?>
+
+
+        <h2>Bus Book <?php echo $book->bus_book_id; ?> | Mission ID: <?php echo $book->mission_id; ?></h2>
+        <h3>Start: <?php echo $book->start; ?> | End: <?php echo $book->end; ?></h3>
+        <?php if(isset($book->notes)) { ?>
+                <p><?php echo $book->notes; ?></p>
+        <?php } ?>
+
+        <table id="<?php echo $book->bus_book_id; ?>book" class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th>Bus ID</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($book->bus as $bus) {?>
+                    <tr>
+                        <td><?php $bus->bus_id ;?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    <?php } 
+    }
 ?>
