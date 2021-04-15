b75c6bd328519e71cd94bfd467c1e95374f43a61
Zach <zacheas@outlook.com>
2021-04-14 21:33:29 -0400

    test

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -47,7 +47,7 @@ class Admin extends CI_Controller {
 	}
 
 	public function buildVetPdf() {
-		$cmd = "python3 scripting/pdf_writer.py";
+		$cmd = "python3 scripting/hello_world.py";
 
 		$test = shell_exec($cmd);
 
--- scripting/hello_world.py
+++ scripting/hello_world.py
@@ -1,1 +1,13 @@
 print("HELLO WORLD!")
+from fpdf import FPDF 
+
+
+class PDF(FPDF):
+   pass
+
+pdf = PDF(orientation='p',unit='mm')
+pdf.add_page()
+
+print("Printing outputting file")
+pdf.output("uploads/HelloWorld.pdf")
+print("HelloWorld.pdf printed")
