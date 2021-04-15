8767193e7734ee02e1011702dbb6a5776a3e028e
Zach <zacheas@outlook.com>
2021-04-14 21:37:05 -0400

    test

--- scripting/hello_world.py
+++ scripting/hello_world.py
@@ -1,13 +1,16 @@
-print("HELLO WORLD!")
-from fpdf import FPDF 
+try:
+    print("HELLO WORLD!")
+    from fpdf import FPDF 
 
 
-class PDF(FPDF):
-   pass
+    class PDF(FPDF):
+    pass
 
-pdf = PDF(orientation='p',unit='mm')
-pdf.add_page()
+    pdf = PDF(orientation='p',unit='mm')
+    pdf.add_page()
 
-print("Printing outputting file")
-pdf.output("uploads/HelloWorld.pdf")
-print("HelloWorld.pdf printed")
+    print("Printing outputting file")
+    pdf.output("uploads/HelloWorld.pdf")
+    print("HelloWorld.pdf printed")
+except Exception e:
+    print("ERROR: " + str(e))
