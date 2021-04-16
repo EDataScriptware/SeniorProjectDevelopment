ab3ab005a8ef975f68a0a8d1f72fad93b6589467
Zach <zacheas@outlook.com>
2021-04-16 01:15:40 -0400

    test

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -132,13 +132,14 @@ class Admin extends CI_Controller {
 	}
 
 	public function deleteTeam($tid, $busid) {
+		echo $tid. ' | ' .$busid;
 		if(isset($tid)) {
 			$this->load->model("Team_model");
 
 			$this->Team_model->deleteTeam($tid);
 		}
 		
-		redirect('admin/editBus/'.$busid) ;
+		//redirect('admin/editBus/'.$busid) ;
 	}
 
 	public function do_upload() {
