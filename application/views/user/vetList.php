2e5534f02adcc32777665f046f4e3ab75c711bb0
0rangeBoy <demonsonicth@gmail.com>
2021-04-19 13:44:43 -0400

    e

--- application/views/user/vetList.php
+++ application/views/user/vetList.php
@@ -2,8 +2,8 @@
 
 <?php // echo json_encode($veteran)?>
 <div class="btn-group">
-<button id = "teamButton" class = "userViewButton" onClick ="showTeam()"> Team View</button>
-<button id = "busButton" class = "userViewButton" onClick ="showBus()"> Bus view </button>
+<button id = "teamButton" class="btn btn-primary" onClick ="showTeam()"> Team View</button>
+<button id = "busButton" class="btn btn-primary" onClick ="showBus()"> Bus view </button>
 </div>
 <div id = "teamView"> 
 
