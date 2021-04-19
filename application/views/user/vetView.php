33a0225eba9c464dd9adbdf6232e752f1d6d5bd2
0rangeBoy <demonsonicth@gmail.com>
2021-04-19 18:37:48 -0400

    t

--- application/views/user/vetView.php
+++ application/views/user/vetView.php
@@ -6,10 +6,10 @@
 <?php $medMedication = array('med_emphysema','med_falls','med_heart_disease','med_pacemaker','med_colostomy','med_cancer','med_dnr','med_hbp','med_joint_replacement','med_kidney', 'med_diabetes','med_seizures','med_urostomy','med_dimentia','med_nebulizer','med_oxygen','med_football','med_stroke','med_urinary','med_cpap') ?>
 
 <div class = "buttonScrollView">
-	<button id = "aboutBut" class = "scrollItem scrollActive" onClick ="showAbout()"> <i class="fa fa-userfa-4x"></i> <br> <b> About </b></button>
-	<button id = "schBut" class = "scrollItem"  onClick ="showRes()"> <i class="fa fa-calendar fa-4x"></i>  <br> <b>  Schedule </b></button>
-	<?php if (in_array($_SESSION["userPerm"], $allowed)) { ?><button id = "medBut" class = "scrollItem" onClick ="showMed()"> <i class="fa fa-medkit fa-4x"></i>  <br> <b> Medical Info </b></button><?php } ?>
-	<?php if (in_array($_SESSION["userPerm"], $allowed)) { ?><button id = "hisBut" class = "scrollItem" onClick ="showAcc()"> <i class="fa fa-history fa-4x"></i>  <br> <b> History </b></button> <?php } ?>
+	<button id = "aboutBut" class = "scrollItem scrollActive" onClick ="showAbout()"> <i class="fa fa-user fa-3x"></i> <br> <b> About </b></button>
+	<button id = "schBut" class = "scrollItem"  onClick ="showRes()"> <i class="fa fa-calendar fa-3x"></i>  <br> <b>  Schedule </b></button>
+	<?php if (in_array($_SESSION["userPerm"], $allowed)) { ?><button id = "medBut" class = "scrollItem" onClick ="showMed()"> <i class="fa fa-medkit fa-3x"></i>  <br> <b> Medical Info </b></button><?php } ?>
+	<?php if (in_array($_SESSION["userPerm"], $allowed)) { ?><button id = "hisBut" class = "scrollItem" onClick ="showAcc()"> <i class="fa fa-history fa-3x"></i>  <br> <b> History </b></button> <?php } ?>
 
 </div>
 
