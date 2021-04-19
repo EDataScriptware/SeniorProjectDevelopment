079dbfb3d572cccef5fecf8d6ca3d56ceeba48c9
0rangeBoy <demonsonicth@gmail.com>
2021-04-19 00:38:43 -0400

    r

--- application/views/user/template/header.php
+++ application/views/user/template/header.php
@@ -32,41 +32,35 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light " style="background-color: #e3f2fd;">
-  <a class="navbar-brand" href="#">
- <img src= "<?php echo base_url('assets/internal/img/icon.png')?>" class="d-inline-block align-top" width="30" height="30" > 
-	  Honor Flight</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<div class="nav-side-menu">
+        <div class="brand">Rochester Honor Flight<img src="img/logo.png" alt="Rochester Honor Flight"> </div>
+        <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
+    
+            <div class="menu-list">
+    
+                <ul id="menu-content" class="menu-content collapse out">
+                    <li>
+                    <a href="#">
+                    <i class="fa fa-dashboard fa-lg"></i> Home Page
+                    </a>
+                    </li>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+                    <li  data-toggle="collapse" data-target="#products" class="collapsed active">
+                    <a href="#"><i class="fa fa-gift fa-lg"></i> Teams <span class="arrow"></span></a>
+                    </li>
+                    <ul class="sub-menu collapse" id="products">
+                        <li class="active"><a href="#">Blue</a></li>
+                        <li><a href="#">Gold</a></li>
+                        <li><a href="#">Green</a></li>
+                        <li><a href="#">Purple</a></li>
+                        <li><a href="#">Purple</a></li>
+                        <li><a href="#">Red</a></li>
+                        <li><a href="#">Silver</a></li>
+                        <li><a href="#">Teal</a></li>
+                        <li><a href="#">Yellow</a></li>
+                    </ul>
+                </ul>
         </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+    </div>
 
+    <div class = "content">
