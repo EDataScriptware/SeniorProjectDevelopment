c45782602b531cd083930e00cc743bf148589175
0rangeBoy <demonsonicth@gmail.com>
2021-04-19 16:25:14 -0400

    t

--- application/views/user/vetList.php
+++ application/views/user/vetList.php
@@ -2,9 +2,9 @@
 
 <?php // echo json_encode($veteran)?>
 <div class="btn-group cnter">
-<button  type="button" id = "teamButton" class="btn btn-dark" onClick ="showTeam()">Team</button>
-<?php if ($id === null) { ?> <button  type="button" id = "busButton" class="btn btn-dark" onClick ="showBus()"> Bus </button> <?php } ?>
-<button  type="button" id = "staffButton" class="btn btn-dark" onClick ="showStaff()"> Staff </button>
+<button  type="button" id = "teamButton" class="btn btn-primary" onClick ="showTeam()">Team</button>
+<?php if ($id === null) { ?> <button  type="button" id = "busButton" class="btn btn-primary" onClick ="showBus()"> Bus </button> <?php } ?>
+<button  type="button" id = "staffButton" class="btn btn-primary" onClick ="showStaff()"> Staff </button>
 </div>
 <div id = "teamView"> 
 
