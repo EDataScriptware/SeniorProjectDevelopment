1354966d09125e255dd8347fb11d5c27660089f7
0rangeBoy <demonsonicth@gmail.com>
2021-04-20 00:55:11 -0400

    r

--- application/views/user/itinerary.php
+++ application/views/user/itinerary.php
@@ -3,6 +3,8 @@
 } ); 
 </script>
 
+<h2> <b> Itinerary </b> </h2>
+
 <div class = "buttonScrollView">
 <?php foreach ($allTeams as $team): ?>
 	<button id = "<?php echo $team->color ?>" class = "scrollItem <?php echo strtolower($team->color) ?>" onClick ="showAbout()"> <i class="fa fa-flag fa-3x"></i> <br> <b> <?php echo $team->color ?> </b></button>
