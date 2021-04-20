4f7de91889861bb6ef9cba67496f98eed38ff81a
0rangeBoy <demonsonicth@gmail.com>
2021-04-19 21:19:32 -0400

    t

--- application/views/user/template/header.php
+++ application/views/user/template/header.php
@@ -26,8 +26,6 @@
 <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.js')?>"> </script>
 <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.bundle.js')?>"> </script>
 
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets\internal\css\internal.css')?>" />
