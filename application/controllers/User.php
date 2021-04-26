e85b096b4e0e002eec8f78f0dd44b27b13a0c5cf
0rangeBoy <demonsonicth@gmail.com>
2021-04-26 14:04:25 -0400

    r

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -83,7 +83,7 @@ class User extends CI_Controller {
 
 		$this->db->select("*");
 		$this->db->from('hotel_info');
-		$this->db->where('guardian_id', $data['veteran']->guardian_id);
+		$this->db->where('guardian_id', $data['veteran'][0]->guardian_id);
 		$data['gHotel'] = $this->db->get()->result();
 
 		$this->db->select_max("mission_id");
