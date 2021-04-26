f1a4f5167218512a2385cc32e6895b9b364649f4
Zach <zacheas@outlook.com>
2021-04-26 18:00:14 -0400

    new itinerary page

--- application/views/user/itinerary.php
+++ application/views/user/itinerary.php
@@ -2,3 +2,15 @@
     $('#itinerary').addClass('active');
 } ); 
 </script>
+<br>
+<h1>Mission Itinerary</h1>
+<hr/>
+<?php if(!file_exists('./uploads/itinerary.pdf')) { ?>
+    <h3>No itinerary found.</h3>
+    <p>Admin must upload a pdf file named: "itinerary" to have a display here.</p>
+<?php } ?>
+
+<?php else { ?>
+    <iframe src=<?php echo base_url("./uploads/itinerary.pdf") ;?> width="100%" height="100%">
+    </iframe>
+<?php } ?>
