99063a741ac4040f7f781db35ebf9827ac01c64a
0rangeBoy <demonsonicth@gmail.com>
2021-04-27 15:47:46 -0400

    r

--- application/views/user/vetList.php
+++ application/views/user/vetList.php
@@ -59,8 +59,8 @@
 		<?php endforeach ?>
 
 		<?php if ($vet->med_code != "") { ?>
-		<p> <b> Med Code: </b> <span class = 'medCircle med<?php echo $vet->med_code ?>' > </span> <?php echo $vet->med_code ?>  <b><?php if ($vet->med_oxygen != 0) { ?> O2 <?php } ?></b>  <b><?php if ($vet->med_cpap != 0) { ?> <span class ='blueSpan'> <i class="fa fa-tint fa-lg"></i> </span>  <?php } ?> </b> </p>
-		<?php  } else { ?><p> <b> Med Code: </b> None  <b><?php if ($vet->med_oxygen != 0) { ?> O2 <?php } ?></b>  <b><?php if ($vet->med_cpap != 0) { ?> <span class ='blueSpan'> <i class="fa fa-tint fa-lg"></i> </span> <?php } ?> </b> </p><?php } ?>
+		<p> <b> Med Code: </b> <span class = 'medCircle med<?php echo $vet->med_code ?>' > </span> <?php echo $vet->med_code ?>  <b><?php if ($vet->med_oxygen != 0) { ?> <span class ='greenSpan'>  O2 </span> <?php } ?></b>  <b><?php if ($vet->med_cpap != 0) { ?> <span class ='blueSpan'> <i class="fa fa-tint fa-lg"></i> </span>  <?php } ?> </b> </p>
+		<?php  } else { ?><p> <b> Med Code: </b> None  <b><?php if ($vet->med_oxygen != 0) { ?><span class ='greenSpan'>  O2 </span> <?php } ?></b>  <b><?php if ($vet->med_cpap != 0) { ?> <span class ='blueSpan'> <i class="fa fa-tint fa-lg"></i> </span> <?php } ?> </b> </p><?php } ?>
 
 </a>
 <?php endforeach ?>
@@ -143,8 +143,8 @@
 	<?php endforeach ?>
 
 	<?php if ($vet->med_code != "") { ?>
-		<p> <b> Med Code: </b> <span class = 'medCircle med<?php echo $vet->med_code ?>' > </span> <?php echo $vet->med_code ?>  <b><?php if ($vet->med_oxygen != 0) { ?> O2 <?php } ?></b>  <b><?php if ($vet->med_cpap != 0) { ?> <span class ='blueSpan'> <i class="fa fa-tint fa-lg"></i> </span>  <?php } ?> </b> </p>
-		<?php  } else { ?><p> <b> Med Code: </b> None  <b><?php if ($vet->med_oxygen != 0) { ?> O2 <?php } ?></b>  <b><?php if ($vet->med_cpap != 0) { ?> <span class ='blueSpan'> <i class="fa fa-tint fa-lg"></i> </span> <?php } ?> </b> </p><?php } ?>
+		<p> <b> Med Code: </b> <span class = 'medCircle med<?php echo $vet->med_code ?>' > </span> <?php echo $vet->med_code ?>  <b><?php if ($vet->med_oxygen != 0) { ?> <span class ='greenSpan'>  O2 </span> <?php } ?></b>  <b><?php if ($vet->med_cpap != 0) { ?> <span class ='blueSpan'> <i class="fa fa-tint fa-lg"></i> </span>  <?php } ?> </b> </p>
+		<?php  } else { ?><p> <b> Med Code: </b> None  <b><?php if ($vet->med_oxygen != 0) { ?> <span class ='greenSpan'>  O2 </span> <?php } ?></b>  <b><?php if ($vet->med_cpap != 0) { ?> <span class ='blueSpan'> <i class="fa fa-tint fa-lg"></i> </span> <?php } ?> </b> </p><?php } ?>
 
 	</a>
 	<?php  }?>
@@ -199,8 +199,8 @@
 	<?php endforeach ?>
 
 		<?php if ($vet->med_code != "") { ?>
-		<p> <b> Med Code: </b> <span class = 'medCircle med<?php echo $vet->med_code ?>' > </span> <?php echo $vet->med_code ?>  <b><?php if ($vet->med_oxygen != 0) { ?> O2 <?php } ?></b>  <b><?php if ($vet->med_cpap != 0) { ?> <span class ='blueSpan'> <i class="fa fa-tint fa-lg"></i> </span>  <?php } ?> </b> </p>
-		<?php  } else { ?><p> <b> Med Code: </b> None  <b><?php if ($vet->med_oxygen != 0) { ?> O2 <?php } ?></b>  <b><?php if ($vet->med_cpap != 0) { ?> <span class ='blueSpan'> <i class="fa fa-tint fa-lg"></i> </span> <?php } ?> </b> </p><?php } ?>
+		<p> <b> Med Code: </b> <span class = 'medCircle med<?php echo $vet->med_code ?>' > </span> <?php echo $vet->med_code ?>  <b><?php if ($vet->med_oxygen != 0) { ?> <span class ='greenSpan'>  O2 </span> <?php } ?></b>  <b><?php if ($vet->med_cpap != 0) { ?> <span class ='blueSpan'> <i class="fa fa-tint fa-lg"></i> </span>  <?php } ?> </b> </p>
+		<?php  } else { ?><p> <b> Med Code: </b> None  <b><?php if ($vet->med_oxygen != 0) { ?> <span class ='greenSpan'>  O2 </span> <?php } ?></b>  <b><?php if ($vet->med_cpap != 0) { ?> <span class ='blueSpan'> <i class="fa fa-tint fa-lg"></i> </span> <?php } ?> </b> </p><?php } ?>
 
 	</a>
 	<?php  }?>
