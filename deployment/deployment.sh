1956bad546b18925956530594c324e0654c87f23
Edward Riley <55459510+emr9018@users.noreply.github.com>
2021-05-02 17:47:43 -0400

    Adds pip3 package requirements and installation

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -2,11 +2,14 @@
 apt-get update
 apt-get -y install < requirements.txt 
 
+# Installs the python3 
+pip3 install -r python_requirements.txt
+
 # Can be git clone once the repository is public
 git clone https://github.com/EDataScriptware/SeniorProjectDevelopment.git
 
 # Rewrites apache2.conf file and restarts the apache2 service.
-cp deployment/apache2.conf /etc/apache2/apache2.conf
+cp apache2.conf /etc/apache2/apache2.conf
 service apache2 restart
 
 # Creates directories.
