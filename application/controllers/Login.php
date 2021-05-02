bb0dacae997494c6fda58628a7fd17eb81bd4a56
Zach <zacheas@outlook.com>
2021-05-01 20:50:13 -0400

    update

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -49,6 +49,7 @@ class Login extends CI_Controller {
 
 				$this->db->select("mission_id");
 				$this->db->where("show_on_front", 1);
+				$this->db->from('mission');
 				$currentMission_id = implode($this->db->get()->row_array());
 
 				// Start a session here
