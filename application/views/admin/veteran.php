3d9f4457e7bb095339ec7d9f6e95af94282cde89
0rangeBoy <demonsonicth@gmail.com>
2021-05-13 14:53:23 -0400

    t

--- application/views/admin/veteran.php
+++ application/views/admin/veteran.php
@@ -22,7 +22,6 @@
             <th>Name</th>
             <th>War(s)</th>
             <th>Date Of Birth</th>
-            <th>On This Mission?</th>
             <th> Team</th>
             <th> Action </th>
         </tr>
@@ -37,7 +36,7 @@
             <td>  <?php if($vet->service_ww2 == 1) { $getter .='World War 2,'; } ?> <?php if($vet->service_korea == 1) { $getter .='Korean War,'; } ?> <?php if($vet->service_cold_war == 1) { $getter .='Cold War,'; } ?> <?php if($vet->service_vietnam == 1) { $getter .='Vietnam,'; } ?>
                 <p> <?php echo substr($getter, 0, -1); ?></p> </td>
             <td><?php echo $vet->dob ?></td>
-            <td><?php if($vet->mission_id == $id) { echo 'Yes'; } else { echo 'No';} ?></td>
+        
             <td><?php if($vet->mission_id == $id) {
                   
             foreach ($team as $tem):
